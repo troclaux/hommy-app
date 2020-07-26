624fd60840a23fdbb2eb571ef6910d20ebce61c8
Arthur <arthur.albuquerque.1995@gmail.com>
2020-07-26 11:31:35 -0300

    # altera o nome da pagina favorites para favoritas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent implements OnInit {
       icon: 'home-outline'
     },
     {
-      title: 'Favorites',
+      title: 'Favoritas',
       url: '/favorites',
       icon: 'heart'
     }    
